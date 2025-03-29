2474208066dacc4d37dd38a7cc4657e05c3d7444
🦜Nannaphat Songduong <121873082+Ouan2551@users.noreply.github.com>
2025-03-29 22:49:56 +0700

    -

--- test.cpp
+++ test.cpp
@@ -1,53 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-class Solution {
-public:
-    string mergeAlternately(string word1, string word2)
-    {
-        string st = "";
-        char A[2*max(word1.size(),word2.size())],B[2*max(word1.size(),word2.size())];
-        for (int i = 0; i < 2*max(word1.size(),word2.size()); i++)
-        {
-            if (i % 2 == 0 && i < 2*word1.size())
-            {
-                A[i] = word1[i/2];
-            }
-            else
-            {
-                A[i] = ' ';
-            }
-            if (i % 2 == 1 && i < 2*word2.size())
-            {
-                B[i] = word2[(i-1)/2];
-            }
-            else
-            {
-                B[i] = ' ';
-            }
-        }
-        for (int i = 0; i < 2*max(word1.size(),word2.size()); i++)
-        {
-            if (A[i] != ' ')
-            {
-                st += A[i];
-            }
-            if (B[i] != ' ')
-            {
-                st += B[i];
-            }
-        }
-        return st;
-    }
-    
-};
-
 int main()
 {
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    Solution so;
-    string a,b;
-    cin >> a >> b;
-    cout << so.mergeAlternately(a,b);
+    char two[3] = {'a', 'b', 'c'}, three[3] = {'d', 'e', 'f'}, four[3] = {'g', 'h', 'i'}, five[3] = {'j', 'k', 'l'},
+    six[3] = {'m', 'n', 'o'}, seven[4] = {'p', 'q', 'r', 's'}, eight[3] = {'t', 'u', 'v'}, nine[4] = {'w', 'x', 'y', 'z'};
     return 0;
 }
---
+++ top_100_rate_problem/Backtracking/17. Letter Combinations of a Phone Number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    public:
+        vector<string> letterCombinations(string digits) {
+            // contain list or num and character
+            char two[3] = {'a', 'b', 'c'}, three[3] = {'d', 'e', 'f'}, four[3] = {'g', 'h', 'i'}, five[3] = {'j', 'k', 'l'},
+            six[3] = {'m', 'n', 'o'}, seven[4] = {'p', 'q', 'r', 's'}, eight[3] = {'t', 'u', 'v'}, nine[4] = {'w', 'x', 'y', 'z'};
+            int count = digits.size();
+            for (int i = 0; i < count; i++)
+            {
+                if (digits[i] == '2')
+                {
+                    /* code */
+                }
+                else if (digits[i] == '3')
+                {
+
+                }
+                else if (digits[i] == '4')
+                {
+
+                }
+                else if ()
+            }
+            
+        }
+    };
