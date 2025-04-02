5a566dc41105a4beac2de937629c965de1977f5c
🦜Nannaphat Songduong <121873082+Ouan2551@users.noreply.github.com>
2025-04-02 22:25:01 +0700

    -

--- top_100_rate_problem/Binary_Search/74.Search_a_2D_Matrix.cpp
+++ top_100_rate_problem/Binary_Search/74.Search_a_2D_Matrix.cpp
@@ -2,29 +2,23 @@
 using namespace std;
 class Solution {
     public:
-        // problem
-        // 1) have something not sure about how to find size vector
-        // because it is not normal vector (1 dimension) it is vector<vector> (2 dimension)
-
-        // 2) declare high value don't need -1 just matrix.size() alone
-        // 3) forget use index compare target instead value (mid == target) lol
-        // 4) don't know how to find mid value for 2d matrix
         bool searchMatrix(vector<vector<int>>& matrix, int target) {
-            int low = 0, high = matrix.size() - 1;
             int m = matrix.size(), n = matrix[0].size();
+            // [0] mean select 1 rows and find columns in rows
+            int low = 0, high = m * n - 1;
             while (low <= high)
             {
                 int mid = low + (high - low)/2;
-                int mid_value = matrix[mid/n][mid%n];
-                if (matrix[mid] == target)
+                int mid_value = matrix[mid / n][mid % n];
+                if (mid_value == target)
                 {
                     return true;
                 }
-                else if (target > matrix[mid])
+                else if (target > mid_value)
                 {
                     low = mid + 1;
                 }
-                else if (target < matrix[mid])
+                else if (target < mid_value)
                 {
                     high = mid - 1;
                 }
