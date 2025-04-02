a628cc86723e04b1f9905fb004df3985049592c6
🦜Nannaphat Songduong <121873082+Ouan2551@users.noreply.github.com>
2025-04-02 23:18:40 +0700

    -

---
+++ top_100_rate_problem/Binary_Search/153._Find_Minimum_in_Rotated_Sorted_Array.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+    public:
+        int findMin(vector<int>& nums) {
+            int count = nums.size(), min_num = INT_MAX;
+            for (int i = 0; i < count; i++)
+            {
+                min_num = min(nums[i], min_num);
+            }
+            return min_num;
+        }
+    };
+int main()
+{
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    Solution solution;
+    return 0;
+}
