9cc9acbec9399e23efbe04cd13003fa2444e47a3
🦜Nannaphat Songduong <121873082+Ouan2551@users.noreply.github.com>
2025-04-03 08:34:45 +0700

    -

---
+++ Learn_for_leetcode/1.Data_structure/1.binary_tree/1.binary_tree_intro.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    // binary tree => non linear and hierarchical (parent and chile relationship) data structure
+    // topmost node call "root", bottom most node call "leaves"
+    // every binary tree have 3 part
+    // 1) Data, 2) Pointer to the left child 3) Pointer to the right child
+    
+    return 0;
+}
---
+++ top_100_rate_problem/Binary_Search/124.Binary_Tree_Maximum_Path_Sum.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    public:
+        int maxPathSum(TreeNode* root) {
+            vector<int> value;
+            for (int i = 0; i < count; i++)
+            {
+                /* code */
+            }
+            
+        }
+    };
+int main()
+{
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    Solution solution1;
+    return 0;
+}
