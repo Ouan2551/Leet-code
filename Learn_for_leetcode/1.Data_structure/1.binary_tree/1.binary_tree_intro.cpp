a067b26556ffb29b22ae8ec2043524f07116c5b0
🦜Nannaphat Songduong <121873082+Ouan2551@users.noreply.github.com>
2025-04-03 09:14:18 +0700

    -

--- Learn_for_leetcode/1.Data_structure/1.binary_tree/1.binary_tree_intro.cpp
+++ Learn_for_leetcode/1.Data_structure/1.binary_tree/1.binary_tree_intro.cpp
@@ -1,5 +1,32 @@
 #include <iostream>
 using namespace std;
+// creat and declare binary tree have 2 way using struct or class
+    // 1) strcut
+    struct  Node
+    {
+        int data;
+        Node* left, Node* right;
+        Node(int key)
+        {
+            data = key;
+            left = nullptr;
+            right = nullptr;
+        }
+    };
+
+    // 2) class
+    class Node
+    {
+    public:
+        int data;
+        Node* left, * right;
+    
+        Node(int key) {
+            data = key;
+            left = nullptr;
+            right = nullptr;
+        }
+    };
 int main()
 {
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
@@ -7,6 +34,7 @@ int main()
     // topmost node call "root", bottom most node call "leaves"
     // every binary tree have 3 part
     // 1) Data, 2) Pointer to the left child 3) Pointer to the right child
+
     
     return 0;
 }
