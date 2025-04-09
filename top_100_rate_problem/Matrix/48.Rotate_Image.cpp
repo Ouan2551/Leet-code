6251ce86adefaff6dc0cbe154cfbe664dce7667f
🦜Nannaphat Songduong <121873082+Ouan2551@users.noreply.github.com>
2025-04-09 22:24:44 +0700

    -

--- top_100_rate_problem/Matrix/48.Rotate_Image.cpp
+++ top_100_rate_problem/Matrix/48.Rotate_Image.cpp
@@ -3,7 +3,27 @@ using namespace std;
 class Solution {
     public:
         void rotate(vector<vector<int>>& matrix) {
-            int 
+            int length = matrix.size();
+            int top = 0, bottom = length - 1;
+            while (top < bottom)
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    int temp = matrix[top][i];
+                    matrix[top][i] = matrix[bottom][i];
+                    matrix[bottom][i] = temp;
+                }
+                top++; bottom--;
+            }
+            for (int row = 0; row < length; row++)
+            {
+                for (int column = row + 1; column < length; column++)
+                {
+                    int temp = matrix[row][column];
+                    matrix[row][column] = matrix[column][column];
+                    matrix[column][row] = temp;
+                }
+            }
         }
     };
 int main()
