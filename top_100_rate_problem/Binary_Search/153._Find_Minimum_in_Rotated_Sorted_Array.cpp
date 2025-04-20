8a90aab709b9a8e81143765d8ace0fbd208e0406
🦜Nannaphat Songduong <121873082+Ouan2551@users.noreply.github.com>
2025-04-20 23:54:26 +0700

    -

--- top_100_rate_problem/Binary_Search/153._Find_Minimum_in_Rotated_Sorted_Array.cpp
+++ top_100_rate_problem/Binary_Search/153._Find_Minimum_in_Rotated_Sorted_Array.cpp
@@ -3,13 +3,32 @@ using namespace std;
 class Solution {
     public:
         vector<int> spiralOrder(vector<vector<int>>& matrix) {
-            int dx = 1, dy = 0;
-            
+            int dx = 1, dy = 0, x_len = matrix.size(), y_len = matrix[0].size(), count = 1, i = 0, j = 0, temp = 0;
+            vector<int> result;
+            while (count <= x_len*y_len)
+            {
+                int value = matrix[i][j];
+                result.push_back(value);
+                if (i + 1 == x_len || j + 1 == y_len)
+                {
+                    temp = dy; dy = dx; dx = -temp;
+                }
+                i += dx; j += dy; count++;
+            }
+            return result;
         }
     };
 int main()
 {
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    Solution solution;
+    vector<vector<int>> matrix = {{1,2,3}, {4,5,6}, {7,8,9}};
+    vector<int> result;
+    Solution solution1;
+    result = solution1.spiralOrder(matrix);
+    int size = result.size();
+    for (int i = 0; i < size; i++)
+    {
+        cout << result[i] << '\n';
+    }
     return 0;
 }
