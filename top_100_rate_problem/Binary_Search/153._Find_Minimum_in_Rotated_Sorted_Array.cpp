edb58347fc3ede648bfc1286f7baa0dbe869f933
🦜Nannaphat Songduong <121873082+Ouan2551@users.noreply.github.com>
2025-04-21 23:08:56 +0700

    -

--- top_100_rate_problem/Binary_Search/153._Find_Minimum_in_Rotated_Sorted_Array.cpp
+++ top_100_rate_problem/Binary_Search/153._Find_Minimum_in_Rotated_Sorted_Array.cpp
@@ -2,33 +2,18 @@
 using namespace std;
 class Solution {
     public:
-        vector<int> spiralOrder(vector<vector<int>>& matrix) {
-            int dx = 1, dy = 0, rows = matrix.size(), columns = matrix[0].size(), count = 0, i = 0, j = 0;
-            vector<int> result;
-            while (count < rows*columns)
+        int findMin(vector<int>& nums) {
+            int count = nums.size(), min_num = INT_MAX;
+            for (int i = 0; i < count; i++)
             {
-                result.push_back(matrix[i][j]);
-                matrix[i][j] = -101;
-                if (i + dy < 0 ||i + dy == rows || j + dx < 0 || j + dx == columns || matrix[i+dy][j+dx] == -101)
-                {
-                    int temp = dy; dy = dx; dx = -temp;
-                }
-                i += dy; j += dx; count++;
+                min_num = min(nums[i], min_num);
             }
-            return result;
+            return min_num;
         }
     };
 int main()
 {
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    vector<vector<int>> matrix = {{1,2,3}, {4,5,6}, {7,8,9}};
     Solution solution1;
-    vector<int> result;
-    result = solution1.spiralOrder(matrix);
-    int size = result.size();
-    for (int i = 0; i < size; i++)
-    {
-        cout << result[i] << '\n';
-    }
     return 0;
 }
