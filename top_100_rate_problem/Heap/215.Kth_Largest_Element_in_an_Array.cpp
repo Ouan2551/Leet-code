a4bb3c1b41ddde142f8e572db25757c87ab0b526
🦜Nannaphat Songduong <121873082+Ouan2551@users.noreply.github.com>
2025-04-26 23:50:30 +0700

    -

--- top_100_rate_problem/Heap/215.Kth_Largest_Element_in_an_Array.cpp
+++ top_100_rate_problem/Heap/215.Kth_Largest_Element_in_an_Array.cpp
@@ -3,10 +3,23 @@ using namespace std;
 class Solution {
     public:
         int findKthLargest(vector<int>& nums, int k) {
-            int count = nums.size();
-            sort(nums.begin(), nums.end());
-            int result = nums[count - k];
-            return result;
+            // 1. sort number easy solution
+            // int count = nums.size();
+            // sort(nums.begin(), nums.end());
+            // int result = nums[count - k];
+            // return result;
+
+            // 2. using queue
+            priority_queue<int, vector<int>, greater<int>> mini_heap;
+            for (int i : nums)
+            {
+                mini_heap.push(i); int size = mini_heap.size();
+                if (size > k)
+                {
+                    mini_heap.pop();
+                }
+            }
+            return mini_heap.top();
         }
     };
 int main()
