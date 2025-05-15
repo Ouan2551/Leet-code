ef557005ef3df473ecec043fa2e6c8e90a154316
🦜Nannaphat Songduong <121873082+Ouan2551@users.noreply.github.com>
2025-05-15 22:28:45 +0700

    -

--- top_100_rate_problem/Heap/347.Top_K_Frequent_Elements.cpp
+++ top_100_rate_problem/Heap/347.Top_K_Frequent_Elements.cpp
@@ -9,16 +9,7 @@ class Solution {
             {
                 check_count[nums[i]] += 1;
             }
-            count = check_count.size();
-            for (int i = 0; i < 2; i++)
-            {
-                int max_count = INT_MIN;
-                for (int j : check_count)
-                {
-                    max_count = max(max_count, j.second)
-                }
-                
-            }
+            
         }
     };
 int main()
